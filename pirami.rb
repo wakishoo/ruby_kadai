d84bf1f5187c821368dc608d58ec38f74d395166
wakisaka <wakisaka@MacBook-Pro-4.local>
2019-04-12 15:46:37 +0900

    first commit

---
+++ pirami.rb
@@ -0,0 +1,26 @@
+input = gets.to_i
+i = input - 1
+j = 1
+
+while i >= 0
+	puts '  ' * i + '*   ' * j
+	i -= 1
+	j += 1
+end
+
+
+
+
+
+# input = gets.to_i
+# i = 1
+# while i<=input do
+# 	puts " "*(input-1) + "*"*i
+# 	i += 1
+# end
+
+
+# puts "   *"
+# puts "  * *"
+# puts " * * *"
+# puts "* * * *"
